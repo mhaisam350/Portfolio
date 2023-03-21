90668a306b5bf12221b37a825e4dbf77d6d0d657
mhaisam350 <mhaisam350@gmail.com>
2023-03-21 12:38:22 +0500

    Created and styled Projects page

--- components/ProjectDisplay.js
+++ components/ProjectDisplay.js
@@ -6,61 +6,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-export default function ProjectDisplay( { index, project } ) {
+export default function ProjectDisplay( { project } ) {
 
     console.log(project);
 
     return (
 
-        index % 2 === 0 ? 
-
-            <article className={'flex' + " " + styles['container']}>
+        <article className={['container']}>
+                
+            <img src={project.image} className={styles['project-image']} alt='Project screenshot' />
             
-                <div className={styles['project-text']}>
+            <div className={styles['project-text']}>
+
+                <div className={'flex' + " " + styles['project-flex']}>
 
                     <h3 className={styles['project-heading']}>{project.title}</h3>
-
-                    <p className={styles['project-paragraph']}>{project.description}</p>
 
                     <div className={styles['project-links']}>
 
                         <Link className={styles.link} href={project.live} target='_blank'><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></Link>
-                        
+                            
                         <Link className={styles.link} href={project.github} target='_blank'><FontAwesomeIcon icon={faGithub} /></Link>
 
                     </div>
-
+                
                 </div>
 
-                <img src={project.image} className={styles['project-image']} alt='Project screenshots' />
+                <p className={styles['project-paragraph']}>{project.description}</p>
 
-            </article>
+                
 
-        : 
+            </div>
 
-            <article className={'flex' + " " + styles['container']}>
+        </article>
 
-                <img src={project.image} className={styles['project-image']} alt='Project screenshots' />
-            
-                <div className={styles['project-text']}>
-
-                    <h3 className={styles['project-heading']}>{project.title}</h3>
-
-                    <p className={styles['project-paragraph']}>{project.description}</p>
-
-                    <div className={styles['project-links']}>
-
-                        <Link className={styles.link} href={project.live} target='_blank'><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></Link>
-                        
-                        <Link className={styles.link} href={project.github} target='_blank'><FontAwesomeIcon icon={faGithub} /></Link>
-
-                    </div>
-
-                </div>
-
-
-            </article>
-
-    );
+    )
 
 }
---
+++ pages/projects.js
@@ -0,0 +1,72 @@
+import { useRef } from 'react';
+
+import Head from 'next/head';
+import Link from 'next/link';
+
+import styles from '@/styles/Projects.module.scss';
+
+// import Navigation from '@/components/Navigation';
+import ProjectDisplay from '@/components/ProjectDisplay';
+
+import Canvas from '@/components/Canvas';
+import Cursor from '@/components/Cursor';
+
+import useEventListener from '@/hooks/useEventListener';
+
+import data from '@/data/projects.json';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { faGit } from '@fortawesome/free-brands-svg-icons';
+
+export default function Projects() {
+
+    const projectsSection = useRef();
+
+    useEventListener(projectsSection);
+
+    const { projects } = data;
+
+    return (
+
+        <>
+        
+            <Head>
+                <title>Haisam | Projects</title>
+                <meta name="description" content="Front End React Developer" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+
+            <Canvas />
+
+            <Cursor />
+
+            <section ref={projectsSection} className={'flex' + " " + styles['projects']}>
+
+                <div>
+
+                    <h2 className={'section-heading'}>Projects</h2>
+
+                    <div className={'grid' + " " + styles['projects-grid']}>
+
+                        {
+
+                            projects.map((project, index) => (
+
+                            <ProjectDisplay key={index} project={project} />
+
+                            ))
+
+                        }
+
+                    </div>
+
+                </div>
+
+            </section>
+
+        </>
+
+    )
+
+}
